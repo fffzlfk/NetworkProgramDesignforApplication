7eaaf96e4ca1aae94e19feed60fb74dbfcaea611
root <root@DESKTOP-U99TE3D.localdomain>
2021-06-16 15:58:47 +0800

    update-net

--- task2/pipe.cc
+++ task2/pipe.cc
@@ -6,10 +6,12 @@
 using namespace std;
 
 int main() {
-    int pipe1[2], pipe2[2];
+    int pipe1[2], pipe2[2], pipe3[2], pipe4[2];
 
-    if (pipe(pipe1) < 0 || pipe(pipe2) < 0) {
+    if (pipe(pipe1) < 0 || pipe(pipe2) < 0 || pipe(pipe3) < 0 ||
+        pipe(pipe4) < 0) {
         perror("could not create pipes.");
+        exit(-1);
     }
 
     pid_t pid;
@@ -28,23 +30,51 @@ int main() {
         if (i == 0) {
             close(pipe1[0]);
             close(pipe2[1]);
+            close(pipe3[0]);
+            close(pipe3[1]);
+            close(pipe4[0]);
+            close(pipe4[1]);
             char msg[] = "child 1 to child 2";
-            write(pipe1[1], msg, sizeof(msg));
-            if (read(pipe2[0], buf, sizeof(buf)) > 0) {
-                printf("%d's child1(%d) received: %s\n", getppid(), getpid(), buf);
+            for (int i = 0; i < 10; i++) {
+                write(pipe1[1], msg, sizeof(msg));
+                if (read(pipe2[0], buf, sizeof(buf)) > 0) {
+                    printf("%d's child 1(%d) received: %s\n", getppid(),
+                           getpid(), buf);
+                }
+                sleep(1);
             }
             exit(0);
         } else if (i == 1) {
-            close(pipe2[0]);
+            close(pipe1[0]);
             close(pipe1[1]);
-            if (read(pipe1[0], buf, sizeof(buf)) > 0) {
-                printf("%d's child2(%d) received: %s\n", getppid(), getpid(), buf);
+            close(pipe2[0]);
+            close(pipe2[1]);
+            close(pipe4[0]);
+            close(pipe3[1]);
+            for (int i = 0; i < 10; i++) {
+                if (read(pipe3[0], buf, sizeof(buf)) > 0) {
+                    printf("%d's child 2(%d) received: %s\n", getppid(),
+                           getpid(), buf);
+                }
+                char msg[] = "child 2 to child 1";
+                write(pipe4[1], msg, sizeof(msg));
+                sleep(1);
             }
-            char msg[] = "child 2 to child 1";
-            write(pipe2[1], msg, sizeof(msg));
             exit(0);
         }
     } else {
+        close(pipe1[1]);
+        close(pipe3[0]);
+        close(pipe2[0]);
+        close(pipe4[1]);
+
+        while (true) {
+            read(pipe1[0], buf, sizeof buf);
+            write(pipe3[1], buf, sizeof buf);
+            read(pipe4[0], buf, sizeof buf);
+            write(pipe2[1], buf, sizeof buf);
+        }
+
         wait(NULL);
         wait(NULL);
     }
